95faf42334d493ae94a607a3f294efff6ffef782
VitorSouza <vsalves0707@gmail.com>
2025-03-24 14:48:19 -0300

    Mexendo

--- src/main/java/com/senai/crud_usuario_spring_arraylist/controller/CrudApplicationController.java
+++ src/main/java/com/senai/crud_usuario_spring_arraylist/controller/CrudApplicationController.java
@@ -1,4 +1,15 @@
 package com.senai.crud_usuario_spring_arraylist.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuarios")
 public class CrudApplicationController {
+
+    @GetMapping
+    public String getRegistros(){
+        return "ListaUsuarios";
+    }
 }
