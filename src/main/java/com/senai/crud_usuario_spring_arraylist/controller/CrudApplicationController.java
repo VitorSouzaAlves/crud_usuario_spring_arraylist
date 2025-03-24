91c5749c4b5adec2ac831bed6bfe207979e2e458
VitorSouza <vsalves0707@gmail.com>
2025-03-24 15:19:49 -0300

    Alterando o retorno na application

--- src/main/java/com/senai/crud_usuario_spring_arraylist/controller/CrudApplicationController.java
+++ src/main/java/com/senai/crud_usuario_spring_arraylist/controller/CrudApplicationController.java
@@ -1,5 +1,6 @@
 package com.senai.crud_usuario_spring_arraylist.controller;
 
+import com.senai.crud_usuario_spring_arraylist.entity.Usuario;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class CrudApplicationController {
 
     @GetMapping
-    public String getRegistros(){
-        return "ListaUsuarios";
+    public Usuario getUsuario(){
+        return new Usuario(1, "Vitor");
     }
 }
---
+++ src/main/java/com/senai/crud_usuario_spring_arraylist/entity/Usuario.java
@@ -0,0 +1,23 @@
+package com.senai.crud_usuario_spring_arraylist.entity;
+
+public class Usuario {
+    private int id;
+    private String nome;
+
+    public Usuario(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
